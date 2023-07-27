3beff4a113ee9985abff15edbf36ab7c70d45cf6
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-27 17:21:19 +0500

    Update MultiplicationTable.java

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        for(int i = 1; i < numberTableToPrint; i++){
+            for(int k = 1; k < numberTableToPrint; k++){
+                System.out.print(k * i + "  ");
+            }
+            System.out.println("");
+        }
     }
 }
