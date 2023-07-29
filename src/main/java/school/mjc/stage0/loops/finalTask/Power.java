0ce886405e59c05433bf157455e9ff7047471ea9
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-29 16:07:19 +0500

    Update Power.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -6,10 +6,11 @@ public class Power {
         for (int i = 1; i <= power; i++) {
             result = result * numberToPrint;
         }
-        System.out.print(result);
+        System.out.println(result);
     }
+}
 
     public static void main(String[] args) {
         new Power().printPower(10, 3);
     }
-}
+
